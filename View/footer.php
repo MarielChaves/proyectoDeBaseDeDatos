b7ae86669e0195828b0dc431c6256f35f43d7a9d
Fabiola <fabiolaalvarado11@gmail.com>
2019-05-09 21:55:03 -0600

    Se agregaron carpetas de Controller y View View contiene el header y footer de la pagina

---
+++ View/footer.php
@@ -0,0 +1,32 @@
+<div class="row">
+    <div class="col-lg-12" id="contFooter">
+        <hr/>
+        <div>
+            <div>
+                <footer class="text-center well" id="inicioFooter">
+                    <p id="derechosReservados">Clinica FCS 2018 &copy; Todos los derechos reservados</p>
+                    <div>
+                        <table id="tablaFooter">
+                            <tr>
+                                <td id="colTablaFooter"><a id="styleLinkFooter" class="linkNav" href="https://www.facebook.com/clinicabiblica/">Facebook</a></td>
+                                <td id="colTablaFooter"><i class="fab fa-facebook-f"></i></td>
+                                <td id="colTablaFooter"><a id="styleLinkFooter" class="linkNav" href="https://twitter.com/clinicabiblica?lang=es">Twitter</a></td>
+                                <td id="colTablaFooter"><i class="fab fa-twitter"></i></td>
+                                <td id="colTablaFooter"><a id="styleLinkFooter" class="linkNav" href="https://www.instagram.com/?hl=es-la">Instagram</a></td>
+                                <td id="colTablaFooter"><i class="fab fa-instagram"></i></td>
+                                <td id="colTablaFooter"><a id="styleLinkFooter" class="linkNav" href="https://www.youtube.com">Youtube</a></td>
+                                <td id="colTablaFooter"><i class="fab fa-youtube"></i></td>
+                            </tr>
+                        </table>
+                    </div>
+                </footer>  
+            </div>            
+        </div>                      
+    </div>    
+</div>
+<script src="Assets/js/jquery-3.3.1.slim.min.js"></script>
+<script src="Assets/js/bootstrap.min.js"></script>
+<script href="Assets/js/ocultarWidget.js" type="text/javascript"></script>
+
+</body>
+</html>
