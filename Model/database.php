6bdc145e1dfd4a3cb6a7d8116db91f1ea4e688ad
loaic <loaiciga38@gmail.com>
2019-05-11 17:24:49 -0600

    probar

--- Model/database.php
+++ Model/database.php
@@ -3,9 +3,10 @@
 class Database {
 
     public static function StartUp() {
-        $pdo = new PDO('mysql:host=localhost;dbname=sistemadecompras;charset=utf8', 'root', '');
+        $pdo = new PDO('mysql:host=localhost;dbname=sistemadecompras;charset=utf8', 'root', '123456');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
+    /*huevqwuhrbw*/
 
 }
