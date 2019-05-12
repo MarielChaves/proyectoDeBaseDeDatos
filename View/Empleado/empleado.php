f61e8cd8a405967f3c174e525da4e665bf75a319
Fabiola <fabiolaalvarado11@gmail.com>
2019-05-12 14:32:15 -0600

    Clase empleado

---
+++ View/Empleado/empleado.php
@@ -0,0 +1,53 @@
+<div class="contTitleService">
+    <div class="subContTitleService">
+        <div id="titleService" class="container">
+            <h1 class="page-header">Empleado</h1>
+        </div>
+    </div>
+</div>
+
+<form action="?c=Empleado" method="post">
+
+    <div class="well well-sm text-right">
+
+        <div class="contLabelBuscar">
+            <label class="labelBuscar">Buscar:</label>
+            <input class="form-control" id="buscar" type="text"  placeholder="Escriba algo para buscar"/>
+        </div>
+
+        <div id="margenCont">
+            <a id="botonRegistrar" class="btn btn-primary" href="?c=Empleado&a=Registrar">Registrar</a>
+            <input id="botonEditar" type="submit" value="Editar" name="a" class="btn btn-primary"/>
+            <input id="botonEliminar" type="submit" value="Eliminar" name="a" onclick="javascript:return confirm('¿Seguro de eliminar este registro?');" class="btn btn-primary"/>
+        </div>
+    </div>
+
+    <table id="tabla" class="table table-striped">
+        <thead>
+            <tr>
+                <th class="spaceCol"></th>
+                <th class="spaceCol">Identificacion</th>
+                <th class="spaceCol">Nombre</th>
+                <th class="spaceCol">Apellidos</th>
+                <th class="spaceCol">Telefono</th>
+                <th class="spaceCol">Tipo de Empleado</th>
+                <th class="spaceCol">Clave</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($this->model->Listar() as $user): ?>
+                <?php $valor = $emple->idEmpleado; ?>
+                <tr>
+                    <td><input id="marginRadio" type=radio name=id value=<?php echo $emple->idEmpleado; ?> ></td>
+                    <td><?php echo $user->Identificacion; ?></td>
+                    <td><?php echo $user->Nombre; ?></td>
+                    <td><?php echo $user->Apellidos; ?></td>
+                    <td><?php echo $user->Telefono; ?></td>
+                    <td><?php echo $user->Tipo; ?></td>
+                    <td><?php echo $user->Clave; ?></td>               
+                </tr>
+            <?php endforeach; ?>
+        <script src="assets/js/buscador.js"></script>
+        </tbody>
+    </table>
+</form>
